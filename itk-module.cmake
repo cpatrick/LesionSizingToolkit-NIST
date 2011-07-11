a1386f1c15421c85b718ea0b4c4450f4ba53512c
Patrick Reynolds <cpreynolds@gmail.com>
2011-07-11 13:25:44 -0400

    BUG: Working around IO Factory registration to allow mha output.

--- itk-module.cmake
+++ itk-module.cmake
@@ -5,4 +5,4 @@ range of applications. The Lesion Sizint Toolkit is build upon ITK and adds to
 it specific classes for computing image features that are then combined into a
 detection and measurement layer.")
 
-itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta ITK-VTK TEST_DEPENDS ITK-TestKernel DESCRIPTION "${DOCUMENTATION}")
+itk_module(ITK-LesionSizingToolkit DEPENDS ITK-ImageFeature ITK-LevelSets ITK-Statistics ITK-IO-SpatialObjects ITK-RegionGrowing ITK-LabelVoting ITK-MathematicalMorphology  ITK-IO-GDCM ITK-IO-Meta ITK-VTK ITK-TestKernel DESCRIPTION "${DOCUMENTATION}")
